d1e33a7f41aa56a7152f8552f1dfec316486e16a
Anna Kaser <kaserann96@gmail.com>
2022-05-01 12:23:13 -0700

    nav and login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 import Home from './components/Home';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <Home></Home>
       </main>
---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <section>
+            <header id="nav">
+                <h2 className="name">
+                    <a href="/recipe-box">
+                        Recipe Box
+                    </a>
+                </h2>
+            </header>
+            <nav className="nav-items">
+                <ul className="nav nav-tabs flex-row">
+
+                </ul>
+            </nav>
+        </section>
+    );
+}
+
+export default Nav;
