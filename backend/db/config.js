77c309dbdc34b58875e3b3f394f840dbc5b85bab
Admin <admin@PL-21277-MacBook.local>
2022-12-28 13:20:50 -0800

    new user added to database

---
+++ backend/db/config.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:recipebox123@recipe-main.wm6jsrt.mongodb.net/RecipeBox');
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -24,6 +24,30 @@ function Home() {
     pass: "invalid password"
   };
 
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+    const json = JSON.stringify(object);
+    console.log(object);
+    console.log(json);
+    const collectData = async () => {
+      let result = await fetch('http://localhost:5000/register', {
+        method: 'post',
+        body: json,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      result = await result.json();
+      console.warn(result);
+    }
+    collectData();
+  }
+
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
@@ -52,12 +76,12 @@ function Home() {
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
-  
+
   // Generate button to switch between register and login forms
   const formSwitch = (
-      <button className="button" onClick={() => setIsRegister(!isRegister)}>
-        {isRegister ? 'Already a Member? Log in' : 'Not registered yet? Join now!'}
-      </button>
+    <button className="button" onClick={() => setIsRegister(!isRegister)}>
+      {isRegister ? 'Already a Member? Log in' : 'Not registered yet? Join now!'}
+    </button>
   );
 
   // JSX code for login form
@@ -83,7 +107,7 @@ function Home() {
 
   const renderRegisterForm = (
     <div className="form">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleRegistration}>
         <div className="input-container">
           <label>First Name </label>
           <input type="text" name="firstName" required />
@@ -93,12 +117,12 @@ function Home() {
           <input type="text" name="lastName" required />
         </div>
         <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
+          <label>Email </label>
+          <input type="text" name="email" required />
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input type="password" name="password" required />
         </div>
         <div className="input-container">
           <label>Confirm Password </label>
